e5c6049023e523d0c9ea491473f6a125c40f6a17
procrasticoder <iamankitmishra07@gmail.com>
2021-12-17 03:00:53 +0530

    initial commit

---
+++ base.js
@@ -0,0 +1,135 @@
+document.getElementsByTagName("header")[0].innerHTML=`
+<nav class="navbar-main navbar navbar-expand-lg navbar-light bg-light px-4 mb-0">
+<div class="container-fluid">
+  <a href="index.php"><img src="static/images/g-g-foods-logo.jpg" alt="" style="height:70px;"></a>
+  <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    
+    <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+      <li class="nav-item mx-3">
+        <a class="nav-link" href="index.php">Home</a>
+      </li>
+      <li class="nav-item mx-3">
+        <a class="nav-link" href="about-us.php">About Us</a>
+      </li>
+      <li class="nav-item dropdown mx-3">
+        <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-expanded="false" style="background-color: red; border-radius: 3px; color: #fff;">Products</a>
+        <ul class="dropdown-menu">
+          <li><a class="dropdown-item" href="#">Dry Fruit Powder</a></li>
+          <li><hr class="dropdown-divider"></li>
+          <li><a class="dropdown-item" href="#">Fruit Powder</a></li>
+          <li><hr class="dropdown-divider"></li>
+          <li><a class="dropdown-item" href="#">Wheat Grass Powder</a></li>
+          <li><hr class="dropdown-divider"></li>
+          <li><a class="dropdown-item" href="#">Organic Tea And Nutritional Powder</a></li>
+          <li><hr class="dropdown-divider"></li>
+          <li><a class="dropdown-item" href="#">Essential Oils</a></li>
+          <li><hr class="dropdown-divider"></li>
+          <li><a class="dropdown-item" href="#">Natural Oil</a></li>
+          <li><hr class="dropdown-divider"></li>
+          <li><a class="dropdown-item" href="#">Walnut Oil</a></li>
+          <li><hr class="dropdown-divider"></li>
+          <li><a class="dropdown-item" href="products-3.php">All Products</a></li>
+        </ul>
+      </li>
+      <li class="nav-item mx-3">
+        <a class="nav-link" href="contact.php">Contact Us</a>
+      </li>
+    </ul>
+  </div>
+</div>
+</nav>
+`
+document.getElementsByTagName('footer')[0].innerHTML=`
+<div class="footer_one">
+<div class="q_links">
+    <h3>
+        Quick Links
+    </h3>
+    <a href="index.php">Home</a>
+    <a href="about-us.php">About Us</a>
+    <a href="products-1.php">Products</a>
+    <a href="contact.php">Contact</a>
+    <div class="footerline"></div>
+</div>
+
+<div class="q_links">
+    <h3>
+        Our Products
+    </h3>
+          <a  href="#">Dry Fruit Powder</a>
+          
+          <a  href="#">Fruit Powder</a>
+          
+          <a href="#">Wheatgrass Powder</a>
+         
+          <a href="#">Organic Tea And Nutritional Powder</a>
+          
+          <a href="#">Essential Oils</a>
+          
+          <a href="#">Natural Oil</a>
+          
+          <a href="#">Walnut Oil</a>
+
+    
+    <div class="footerline"></div>
+</div>
+
+<div class="q_contact">
+    <h3>
+        Contact Us
+    </h3>
+    <div class="q_contact_link">
+        <i class="fa fa-home"></i>
+        <p>NH- 76, Village Vana Via Kheroda, Tehsil Vallabhnagar, Udaipur-313602, Rajasthan, India</p>
+    </div>
+
+    <div class="q_contact_link">
+        <i class="fa fa-phone"></i>
+        <p>+91-9999999999</p>
+    </div>
+    <div class="q_contact_link">
+        <i class="fa fa-envelope"></i>
+        <a href="https://www.gmail.com" style="text-decoration: underline; cursor: pointer;">dummymail.com</a>
+    </div>
+    <div class="footerline"></div>
+
+</div>
+
+<div class="q_follow_us">
+    <h3>
+        Follow Us
+    </h3>
+    <div class="q_follow_us_link">
+        <i class="fa fa-facebook"></i>
+        <p>facebook</p>
+    </div>
+
+    <div class="q_follow_us_link">
+        <i class="fa fa-instagram"></i>
+        <p>Instagram</p>
+    </div>
+    <div class="q_follow_us_link">
+        <i class="fa fa-youtube"></i>
+        <p>YouTube</p>
+    </div>
+    <div class="q_follow_us_link">
+        <i class="fa fa-linkedin"></i>
+        <p>Linkedln</p>
+    </div>
+    <div class="footerline"></div>
+
+</div>
+</div>
+<div class="footer_two">
+<div class="footer_content">
+    All Rights Reserved. Labdhi Petrochem (Terms of Use)<br>
+                    <a href="https://adjointindia.com/" style="color: inherit; cursor: pointer;">Powered by AdJoint India</a>
+</div>
+<div class="footer_logo">
+    <img style="cursor:pointer;cursor:hand" src="https://seal.starfieldtech.com/images/3/en/siteseal_sf_3_h_l_m.gif"  alt="SSL site seal - click to verify">
+</div>
+</div>
+`
